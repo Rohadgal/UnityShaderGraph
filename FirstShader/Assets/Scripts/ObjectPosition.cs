11ff451a82b213037a7a90380c0dfaa00a001a36
Rohadgal <gal.frias@gmail.com>
2023-10-06 15:13:04 -0600

    running water shader finished

---
+++ FirstShader/Assets/Scripts/ObjectPosition.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ObjectPosition : MonoBehaviour
+{
+    void Update()
+    {
+        Shader.SetGlobalVector("_ObjectPosition", new Vector4(this.transform.position.x, this.transform.position.y, this.transform.position.z, this.transform.localScale.x));        
+    }
+}
